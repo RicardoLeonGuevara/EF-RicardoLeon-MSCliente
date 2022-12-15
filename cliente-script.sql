7e8cf51340124cd4da7982f75c4a12fbdcdb2576
RicardoLeonGuevara <110881039+RicardoLeonGuevara@users.noreply.github.com>
2022-12-14 19:31:13 -0500

    Add files via upload

---
+++ cliente-script.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO pizzeriadb.cliente VALUES (1,'Leon','Av Brasil 1234','Ricardo','980767572');
+INSERT INTO pizzeriadb.cliente VALUES (2,'Salas','Av Venezuela 677','Veronica','970605040');
+INSERT INTO pizzeriadb.cliente VALUES (3,'Garcia','Av Bolivar 468','Pedro','920304455');
+INSERT INTO pizzeriadb.cliente VALUES (4,'Rojas','Calle Lima 342','Luis','950334411');
+
+SELECT * FROM pizzeriadb.cliente;
